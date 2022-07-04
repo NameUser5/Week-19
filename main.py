bed0249b00230e8b49ab075cfda3f0b5ff46a3d0
NameUser5 <sdaut001@fiu.edu>
2022-07-04 16:20:23 -0400

    Four down, three to go
    Will go back and define an "append to list" function, in order to shorten the program, but I am typing all functions manually now-- for the practice. :]

--- main.py
+++ main.py
@@ -13,20 +13,44 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # Scrape and print data for Panama only.
 # Scrape and print the capitals that start with the letter 'd'
 
-# Scrape and print the title of the page -- DONE
-title = soup.title.text
-print(title)
+# # Scrape and print the title of the page -- DONE
+# title = soup.title.text
+# print(title)
 
-# Note: this prints the entire span, including the tags
-pop = soup.select(".country-population")
-print(pop)
 
-# Scrape the population of each country into a list -- DONE
+# # Scrape the population of each country into a list -- DONE
+#
+# population_list = []
+# population = soup.find_all('span',{'class':'country-population'})
+# for _ in population:
+#     population_list.append(_.text)
+#
+# for _ in population_list:
+#     print(_)
 
-population_list = []
-population = soup.find_all('span',{'class':'country-population'})
-for _ in population:
-    population_list.append(_.text)
+# # What NOT to do--> Note: this prints the entire span, including the tags
+# pop = soup.select(".country-population")
+# print(pop)
 
-for _ in population_list:
-    print(_)
+
+# Scrape and print the second hyperlink on the site. -- DONE (?)
+
+hyperlink_list = []
+hyperlinks = soup.find_all('a')
+for _ in hyperlinks:
+    hyperlink_list.append(_)
+
+# Taking the entire tag instead of just the text , so I can see what is being linked.S
+print(hyperlink_list[1])
+
+
+# How many h3 tags are on the site -- DONE
+
+heading_three_list = []
+headings = soup.find_all('h3')
+count = 0
+for _ in headings:
+    heading_three_list.append(_)
+    count += 1
+
+print(f"There are {count} h3 headings on this page.")
